2ce50dbd7b16a9e82d549afe509f3d1f3e59f445
dfaveris <scenaristeur@gmail.com>
2019-05-10 17:37:35 +0200

    services

--- services/solid.auth.service.js
+++ services/solid.auth.service.js
@@ -1,16 +1,6 @@
 var SolidAuthService = class {
   constructor() {
-    this.session = {};
-
-    var auth = this;
-
-this.trackSession();
-  }
-  getSession() {
-    return this.session;
-  }
-trackSession(){
-
+    this.session = {}
     solid.auth.trackSession(session => {
       if (!session){
         console.log('The user is not logged in')
@@ -22,7 +12,40 @@ trackSession(){
       }
 
     })
-      console.log("sess",this.session)
+this.foo(3);
+this.track();
+console.log("SESSION", this.session)
+
   }
+  getSession() {
+    return this.session;
+  }
+
+
+  async foo(value) {
+    const data = await this.bar();
+    data.key = value;
+    console.log(data)
+    return data;
+  }
+  async bar() {
+    console.log("bar")
+    return {foo:1, bar:2}
+  }
+
+  async track() {
+  solid.auth.trackSession(session => {
+    if (!session){
+      console.log('The user is not logged in')
+      this.session.webId = "blop";
+      console.log("s",this.session)
+    }else{
+      console.log(`The user is ${session.webId}`)
+      this.session = session;
+    }
+
+  })
+}
+
 
 }
