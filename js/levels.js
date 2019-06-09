f9988699aa10834525558cdcda6f33275d598921
unknown <scenaristeur@gmail.com>
2019-06-09 19:25:10 +0200

    supprimer nouveau graphe level

--- js/levels.js
+++ js/levels.js
@@ -2,17 +2,17 @@ var level;
 
 var levels = {
   0: {nom: "Créer un nouveau graphe | Create a new graph", lien: "https://youtu.be/YqatYoxz5VU",video: "https://youtu.be/YqatYoxz5VU"},
-  1: {nom: "Créer un nouveau noeud dans le graphe", lien: "https://youtu.be/XkI6cJPUTuk",video: "https://youtu.be/XkI6cJPUTuk"},
-  2: {nom: "Créer un deuxième noeud dans le graphe",lien: "https://youtu.be/AmDwlkLk7f8",video: "https://youtu.be/AmDwlkLk7f8"},
-  3: {nom: "Créer un lien entre deux noeuds", lien: "https://youtu.be/ND8fK2liSdE",video: "https://youtu.be/ND8fK2liSdE"},
-  4: {nom: "Créer un triplet", lien: "https://youtu.be/tXfX8vgfdiQ",video: "https://youtu.be/tXfX8vgfdiQ"},
-  5: {nom: "Créer un graphe avec au moins cinq noeuds et 7 liens", lien: "https://github.com/scenaristeur/spoggy-simple/blob/master/README.md",video: "https://github.com/scenaristeur/spoggy-simple/blob/master/README.md"},
+  1: {nom: "Créer un nouveau noeud dans le graphe | Create a new node into the graph", lien: "https://youtu.be/XkI6cJPUTuk",video: "https://youtu.be/XkI6cJPUTuk"},
+  2: {nom: "Créer un deuxième noeud dans le graphe | Create a second node into the graph",lien: "https://youtu.be/AmDwlkLk7f8",video: "https://youtu.be/AmDwlkLk7f8"},
+  3: {nom: "Créer un lien entre deux noeuds | Create a link between to nodes" , lien: "https://youtu.be/ND8fK2liSdE",video: "https://youtu.be/ND8fK2liSdE"},
+  4: {nom: "Créer un triplet | Create a triplet", lien: "https://youtu.be/tXfX8vgfdiQ",video: "https://youtu.be/tXfX8vgfdiQ"},
+  5: {nom: "Créer un graphe avec au moins cinq noeuds et 7 liens | Create a graph with at least five nodes and seven links", lien: "https://github.com/scenaristeur/spoggy-simple/blob/master/README.md",video: "https://github.com/scenaristeur/spoggy-simple/blob/master/README.md"},
 
 };
 
 
 function increaseLevel(){
-  level = 6 // temporaire affiche tous les tutos tant que ce n'est pas ok
+  level = 5 // temporaire affiche tous les tutos tant que ce n'est pas ok
   //level++;
   //console.log(level)
   localStorage.setItem('level',level)
@@ -25,7 +25,7 @@ function initLevel(){
   document.getElementById("level").textContent  = level;*/
   var x=document.getElementById("tutos-levels");
   x.innerHTML = "";
-  level = 6
+  level = 5
 
   for (var i  = 0; i <= level && i <= Object.keys(levels).length; i++)
   {
