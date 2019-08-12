aefe0e7409e2784d60e2e8aa94019d440a52b4fa
scenaristeur@gmail.com <scenaristeur@gmail.com>
2019-08-12 18:37:16 +0200

    get start

--- shexy/shexy-lit/app/shexy-forms.js
+++ shexy/shexy-lit/app/shexy-forms.js
@@ -4,41 +4,51 @@ import { LitElement, html, property, customElement }  from 'https://unpkg.com/li
 class ShexyForms extends LitElement {
   static get properties() {
     return {
-      schema: { type: String      ,
+      schema: { type: String   /*   ,
         hasChanged(newVal, oldVal) {
-          console.log(newVal, oldVal)
-        }
-      }
-    };
-  }
+        console.log(newVal, oldVal)
+      }*/
+    }
+  };
+}
 
-  constructor() {
-    super();
-    this.name = 'World';
-  }
+constructor() {
+  super();
+  this.name = 'World';
+}
 
-  render() {
-    return html`
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+render() {
+  return html`
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-    <p>Hello, ${this.name}!</p>
-    <div class="card-panel teal lighten-2">shexy forms</div>
-    `;
-  }
+  <p>Hello, ${this.name}!</p>
+  <div class="card-panel teal lighten-2">shexy forms</div>
+  `;
+}
 
-  shouldUpdate(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      console.log(`${propName} changed. oldValue: ${oldValue}`);
-    });
-    return changedProperties.has('schema');
+shouldUpdate(changedProperties) {
+  changedProperties.forEach((oldValue, propName) => {
+    console.log(`${propName} changed. oldValue: ${oldValue}`);
+  });
+  if (changedProperties.has('schema')){
+    this.processShapes()
   }
-  /*
-  var shapes = this.schema.shapes;
-  var start = this.schema.start;
-  console.log("START",start)
-  for (let [url, constraint] of Object.entries(shapes)) {
-  console.log(url)
-  //  Shape(url,constraint)
+  return changedProperties.has('schema');
+}
+
+processShapes(){
+  var schema = JSON.parse(this.schema)
+  console.log(schema)
+  console.log(schema.start)
+}
+
+/*
+var shapes = this.schema.shapes;
+var start = this.schema.start;
+console.log("START",start)
+for (let [url, constraint] of Object.entries(shapes)) {
+console.log(url)
+//  Shape(url,constraint)
 }*/
 }
 
