f6c6d1ab47df999f002db96f2729c8421c4370f4
scenaristeur@gmail.com <scenaristeur@gmail.com>
2019-08-13 16:08:04 +0200

    style

--- shexy/shexy-lit/app/app.js
+++ shexy/shexy-lit/app/app.js
@@ -22,6 +22,7 @@ class SimpleGreeting extends LitElement {
 
   render() {
     return html`
+  
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
     <!--
     <p>Hello, ${this.name}!</p>
@@ -29,7 +30,16 @@ class SimpleGreeting extends LitElement {
 
 
 
-
+    <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">Shexy</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
     <div class="card-panel teal lighten-2">head</div>
 
 
@@ -79,6 +89,33 @@ class SimpleGreeting extends LitElement {
 
 
     <div class="card-panel teal lighten-2">footer</div>
+
+    <footer class="page-footer">
+             <div class="container">
+               <div class="row">
+                 <div class="col l6 s12">
+                   <h5 class="white-text">Footer Content</h5>
+                   <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                 </div>
+                 <div class="col l4 offset-l2 s12">
+                   <h5 class="white-text">Links</h5>
+                   <ul>
+                     <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                     <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                     <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                     <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                   </ul>
+                 </div>
+               </div>
+             </div>
+             <div class="footer-copyright">
+               <div class="container">
+               © 2014 Copyright Text
+               <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+               </div>
+             </div>
+           </footer>
+
     `;
   }
 
--- shexy/shexy-lit/app/shexy-forms.js
+++ shexy/shexy-lit/app/shexy-forms.js
@@ -42,326 +42,269 @@ render() {
 
   const getShape = (shape) => html `
   <fieldset>
-  <legend>  ${this.incremente()} -------------------SHAPE ${this.localName(shape.url)}; <br></legend>
+  <legend>  ${this.incremente()} -------------------SHAPE ${this.localName(shape.url)}</legend>
   <!--  ${this.toText(shape)}-->
   </br>
   ${getConstraint(shape.constraint)}
-  <paper-button
-  class="waves-effect waves-light btn modal-trigger"
-  raised
-
-  >Submit</paper-button>
+  <paper-button class="waves-effect waves-light btn-large modal-trigger" type="submit" raised >Submit ${this.localName(shape.url)} <i class="material-icons right">send</i></paper-button>
   </fieldset>
   `
 
   const getConstraint = (constraint) => html`
+  ${constraint.type ?
+    html ``:html`type non défini : ${this.toText(constraint)}<br>`
+  }
 
-  ${constraint.type
-    ? html ``:html`type non défini : ${this.toText(constraint)}<br>`}
+
+  ${this.isFieldset(constraint.type)
+    ? html `
+    <fieldset>
+    <legend>Constraint Type :${constraint.type} <span title="${this.toText(constraint)}">?</span></legend>
+    `
+    :html ``
+  }
+
+  ${constraint.expression
+    ? html`
+    <span title="Expression ${this.toText(constraint.expression)}"> ? </span>
+    ${getConstraint(constraint.expression)}
+    `
+    : html``
+  }
+
+  ${constraint.expressions
+    ? html` <span title="Expressions ${this.toText(constraint.expressions)}"> ? </span>
+    ${constraint.expressions.map(i => html`${getConstraint(i)}`)}`
+    : html``
+  }
+
+  ${constraint.predicate
+    ? html`<br><span title="${this.toText(constraint)}">${constraint.predicate} :<br></span>`
+    : html``
+  }
+
+  ${constraint.valueExpr
+    ? html`
+    <span title="${this.toText(constraint.valueExpr)}"> ? </span>
+    ${getConstraint(constraint.valueExpr)}`
+    : html``
+  }
+
+  ${constraint.datatype
+    ? html`
+    ${constraint.datatype.endsWith("date")
+    ? html `
+    <input type="date" class="teal lighten-2"
+    placeholder="${constraint.datatype}"
+    title="${constraint.datatype}"
+    ></input>`
+    : html `
+    <input type="text" class="validate teal lighten-2"
+    placeholder="${constraint.datatype}"
+    title="${constraint.datatype}"
+    label="${constraint.datatype}"
+    ></input>`
+  }`
+  : html``
+}
+
+${constraint.shapeExprs
+  ? html`
+  <div title="${this.toText(constraint)}">
+  ${constraint.shapeExprs.map(
+    i => html`
+    ${getConstraint(i)}
+    ${constraint.type == "ShapeOr"
+    ? html ` OR `
+    : html ``
+  }`
+)}
+</div>`
+: html``
+}
 
 
-    ${this.isFieldset(constraint.type)
-      ? html `
-      <fieldset>
-      <legend>${this.incremente()} Constraint Type :${constraint.type} <span title="${this.toText(constraint)}">?</span></legend>
-      `
-      :html `
-      <span title="${this.toText(constraint)}">?</span><br>
-      `
+${constraint.nodeKind
+  ? html`
+  <input
+  type="text" class="validate teal lighten-2"
+  title="${constraint.nodeKind}"
+  placeholder="${constraint.nodeKind}"
+  ></input>
+  ${getMinMax(constraint)}`
+  : html``
+}
+
+${constraint.reference
+  ? html`
+  <paper-button class="waves-effect waves-light btn modal-trigger"
+  @click="${(e) =>this.displayForm(constraint.reference)}"
+  raised>
+  ${constraint.reference}
+  </paper-button>`
+  : html``
+}
+
+
+${constraint.values
+  ? html`
+  <select class="teal lighten-2"
+  @change=${this.selectorChange}
+  title="${this.toText(constraint)}">
+  ${constraint.values.map(i => html`
+    <option value="${i.value}"  >${i.value || i}</option>
+    `)}
+    </select>`
+    : html``
+  }
+
+  ${getMinMax(constraint)}
+
+  ${this.isFieldset(constraint.type)
+    ? html `</fieldset> `
+    :html ``
+  }
+  `
+
+
+  return html`
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+  <style>
+  select {
+    display: block; # obligé car materializecss n'arrive pas à initilaiser les selects
+  }
+  </style>
+
+
+  <div class="section">
+  <h5>Forms</h5>
+  <div class="row center-align">
+  ${this.shapes.map(i => html`
+    <div   class="card-panel hoverable col s12 m6 l3 teal lighten-2">
+    <p title=${i.url} @click="${(e) =>this.panelClicked(i)}">
+    ${this.localName(i.url)}</p>
+    </div>`
+  )}
+  </div>
+  </div>
+
+  <div class="divider"></div>
+
+  <div id="currentShape">
+  ${this.currentShape.url}
+  </div>
+
+  <div class="divider"></div>
+
+  ${this.shapes.map(shape => html`
+    ${getShape(shape)}
+
+    `)}
+
+    <div class="section">
+    <h5>Footprints</h5>
+    <div class="row center-align">
+    ${this.footprint_shapes.map(i => html`
+      <div  class="card-panel hoverable col s12 m6 l3 teal lighten-4">
+      <p title=${i.url}>
+      ${this.localName(i.url)}</p>
+      </div>`
+    )}
+    </div>
+    </div>
+    `;
+  }
+
+
+
+
+  shouldUpdate(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      console.log(`${propName} changed. oldValue: ${oldValue}`);
+    });
+    if (changedProperties.has('schema')){
+      this.processShapes()
+    }
+    return changedProperties.has('schema') || changedProperties.has('currentShape');
+  }
+
+  processShapes(){
+    var app = this;
+    var schema = JSON.parse(this.schema)
+    console.log(schema)
+    console.log(schema.start)
+    var shapes = schema.shapes
+    this.shapes = []
+    this.counter = 0;
+    this.footprint_shapes = []
+    console.log(this.shapes)
+
+    for (let [url, constraint] of Object.entries(shapes)) {
+      console.log(url)
+      var shap = {}
+      shap.url = url;
+      shap.constraint = constraint
+      if(url.endsWith("_Footprint")){
+        app.footprint_shapes = [...app.footprint_shapes, shap]
+      }else{
+        app.shapes = [...app.shapes, shap]
+      }
+      this.currentForm = app.shapes[0]
+    }
+    console.log("SHSHSHSHS",app.shapes)
+  }
+
+
+  localName(uri){
+    var ln = uri;
+    if (uri.lastIndexOf("#") != -1) {
+      ln = uri.substr(uri.lastIndexOf("#")).substr(1)
+    }else{
+      ln = uri.substr(uri.lastIndexOf("/")).substr(1)
+    }
+    return ln
+  }
+
+  panelClicked(shape){
+    console.log(shape)
+    this.currentShape = shape
+  }
+
+  isNotCurrent(shape){
+    if (shape.url == this.currentShape.url){
+      return false
+    }else{
+      return true
+    }
+  }
+
+  toText(json){
+    if (json != undefined){
+    //  console.log("ANALYSE DE TYPE ", json.type, "url :",json.url, "DATA :",json)
+      return JSON.stringify(json, null, 2)
+    }
+    else {
+      console.log("json undefined");
+      return undefined}
+
+    }
+    incremente(){
+      console.log(this.counter)
+      this.counter = this.counter+1
+
+      return this.counter
+    }
+    displayForm(id){
+      console.log("displayForm",id)
     }
 
-    ${constraint.expression
-      ? html`
-      ${this.counter} ~~~~~~~Expression : <span title="${this.toText(constraint.expression)}"> ? </span><br>
-      ${getConstraint(constraint.expression)}
-      <br>
-      `
-      : html``
-    }
-    ${constraint.expressions
-      ? html`${this.counter} ~~~~~~~Expressions :  <span title="${this.toText(constraint.expressions)}"> ? </span><br>
+    isFieldset(shapeType){
 
-      ${constraint.expressions.map(i => html`
-
-        ${getConstraint(i)}<br>
-
-        <br>`)}
-        `
-        : html``
-      }
-
-
-      ${constraint.predicate
-        ? html`${constraint.predicate} :
-        `
-        : html``
-      }
-
-
-      ${constraint.valueExpr
-        ? html`
-        ${getConstraint(constraint.valueExpr)}<span title="${this.toText(constraint.valueExpr)}"> ? </span>
-        <br>
-        `
-        : html``
-      }
-
-      ${constraint.datatype
-        ? html`
-
-        ${constraint.datatype.endsWith("date")
-        ? html `
-        <input
-        type="date" class="validate"
-        placeholder="${constraint.datatype}"
-        title="${constraint.datatype}"
-        ></input>
-        `
-        : html `
-        <input
-        type="text" class="validate"
-        placeholder="${constraint.datatype}"
-        title="${constraint.datatype}"
-        label="${constraint.datatype}"
-        ></input>
-        `
-      }
-      `
-      : html``
+      return shapeType != "Shape" && shapeType != "TripleConstraint" && shapeType != "NodeConstraint" && shapeType != "EachOf" && shapeType != "ShapeRef"
     }
 
+  }
 
-    ${constraint.shapeExprs
-      ? html`${this.counter} ~~~~~~~shapeExprs : <span title="${this.toText(constraint.shapeExprs)}"> ? </span><br>
-      ${constraint.shapeExprs.map(i => html`
-
-        ${getConstraint(i)}<br>
-
-        <br>`)}
-        <br>
-        `
-        : html``
-      }
-
-
-      ${constraint.nodeKind
-        ? html`
-        <div class="input-field col s6">
-        <input
-        type="text" class="validate"
-        placeholder="${constraint.nodeKind}"
-        ></input>
-        ${getMinMax(constraint)}
-
-        <span title="${constraint.nodeKind}"> ? </span>
-        </div>
-        <br>
-        `
-        : html``
-      }
-
-      ${constraint.reference
-        ? html` <paper-button   class="waves-effect waves-light btn modal-trigger"  @click="${(e) =>this.displayForm(constraint.reference)}" raised>${constraint.reference}</paper-button><br>
-        `
-        : html``
-      }
-
-
-      ${constraint.values
-        ? html`${this.counter} ~~~~~~~values <span title="${this.toText(constraint.values)}"> ? </span> : <br>
-        ${constraint.values}
-        ${this.toText(constraint.values)}
-        <select class="teal lighten-2" @change=${this.selectorChange}>
-        ${constraint.values.map(i => html`
-
-          <option value="${i.value}"  >${i.value || i}</option>
-          `)}
-          </select>
-          `
-          : html``
-        }
-
-        ${getMinMax(constraint)}
-        <br>
-
-        ${this.isFieldset(constraint.type)
-          ? html `
-          </fieldset>
-          `
-          :html ``
-        }
-
-        `
-
-
-        return html`
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-        <style>
-        select {
-          display: block; # obligé car materializecss n'arrive pas à initilaiser les selects
-        }
-        </style>
-        <div class="section">
-        <h5>Forms</h5>
-        <div class="row center-align">
-
-        ${this.shapes.map(i => html`
-          <div   class="card-panel hoverable col s12 m6 l3 teal lighten-2">
-          <p title=${i.url} @click="${(e) =>this.panelClicked(i)}">
-          ${this.localName(i.url)}</p>
-
-          <!--  <paper-button
-          class="waves-effect waves-light btn modal-trigger"
-          raised @click=${(e) =>this.panelClicked(i)}
-          title=${i.url}
-          >${this.localName(i.url)}</paper-button>-->
-          </div>
-          `)}
-
-          </div>
-          </div>
-
-          <div class="divider"></div>
-          <div id="currentShape">
-          ${this.currentShape.url}
-          </div>
-          <div class="divider"></div>
-
-          <div class="section">
-          <h5>DATA</h5>
-          <div class="row center-align">
-
-          </div>
-          </div>
-
-
-
-          <div class="divider"></div>
-
-          ${this.shapes.map(shape => html`
-
-            ${getShape(shape)}
-            <br>
-            <shexy-formulaire
-            .url=${shape.url}
-            .constraint = ${shape.constraint}
-            .currentUrl = ${this.currentShape.url}
-            ?hidden=${this.isNotCurrent(shape)}
-            ></shexy-formulaire>
-
-
-            <br>
-            `)}
-            </ul>
-
-
-            <div class="section">
-            <h5>Footprints</h5>
-            <div class="row center-align">
-
-            ${this.footprint_shapes.map(i => html`
-              <div  class="card-panel hoverable col s12 m6 l3 teal lighten-4">
-              <p title=${i.url}>
-              ${this.localName(i.url)}</p>
-
-              </div>
-              `)}
-              </div>
-              </div>
-
-
-
-              `;
-            }
-
-
-
-
-            shouldUpdate(changedProperties) {
-              changedProperties.forEach((oldValue, propName) => {
-                console.log(`${propName} changed. oldValue: ${oldValue}`);
-              });
-              if (changedProperties.has('schema')){
-                this.processShapes()
-              }
-              return changedProperties.has('schema') || changedProperties.has('currentShape');
-            }
-
-            processShapes(){
-              var app = this;
-              var schema = JSON.parse(this.schema)
-              console.log(schema)
-              console.log(schema.start)
-              var shapes = schema.shapes
-              this.shapes = []
-              this.counter = 0;
-              this.footprint_shapes = []
-              console.log(this.shapes)
-
-              for (let [url, constraint] of Object.entries(shapes)) {
-                console.log(url)
-                var shap = {}
-                shap.url = url;
-                shap.constraint = constraint
-                if(url.endsWith("_Footprint")){
-                  app.footprint_shapes = [...app.footprint_shapes, shap]
-                }else{
-                  app.shapes = [...app.shapes, shap]
-                }
-                this.currentForm = app.shapes[0]
-              }
-              console.log("SHSHSHSHS",app.shapes)
-            }
-
-
-            localName(uri){
-              var ln = uri;
-              if (uri.lastIndexOf("#") != -1) {
-                ln = uri.substr(uri.lastIndexOf("#")).substr(1)
-              }else{
-                ln = uri.substr(uri.lastIndexOf("/")).substr(1)
-              }
-              return ln
-            }
-
-            panelClicked(shape){
-              console.log(shape)
-              this.currentShape = shape
-            }
-
-            isNotCurrent(shape){
-              if (shape.url == this.currentShape.url){
-                return false
-              }else{
-                return true
-              }
-            }
-
-            toText(json){
-              if (json != undefined){
-                console.log("ANALYSE DE TYPE ", json.type, "url :",json.url, "DATA :",json)
-                return JSON.stringify(json, null, 2)
-              }
-              else {
-                console.log("json undefined");
-                return undefined}
-
-              }
-              incremente(){
-                console.log(this.counter)
-                this.counter = this.counter+1
-
-                return this.counter
-              }
-              displayForm(id){
-                console.log("displayForm",id)
-              }
-
-              isFieldset(shapeType){
-
-                return shapeType != "Shape" && shapeType != "TripleConstraint" && shapeType != "NodeConstraint" && shapeType != "EachOf" && shapeType != "ShapeRef"
-              }
-
-            }
-
-            customElements.define('shexy-forms', ShexyForms);
+  customElements.define('shexy-forms', ShexyForms);
